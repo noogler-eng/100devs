bca2aa656d1b40e99394e5c125475cbd2d76d988
noogler-eng <sharadpoddar10102003@gmail.com>
2025-03-27 11:58:48 +0530

    state-managment

---
+++ singeton-patterns/state-managment/backend/src/logger.ts
@@ -0,0 +1,2 @@
+// this will keep updating the data in database in regualr
+// interval of time
---
+++ singeton-patterns/state-managment/backend/src/store.ts
@@ -0,0 +1,2 @@
+// this is fro the in memory which decrease the latency in our 
+// realtime gameplay or application
