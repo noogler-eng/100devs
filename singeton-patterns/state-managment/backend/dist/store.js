1200348a1bd5acf019095cc9f09cecc9b467df56
noogler-eng <sharadpoddar10102003@gmail.com>
2025-03-27 12:34:23 +0530

    state

--- singeton-patterns/state-managment/backend/dist/store.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// this is fro the in memory which decrease the latency in our
-// realtime gameplay or application
-Object.defineProperty(exports, "__esModule", { value: true });
-[];
-const games = [];
-exports.default = games;
